eaf15e8a006b4f041debb68306daa259b3ee9659
modulitos <modulitos@users.noreply.github.com>
2020-08-16 01:21:29 -0700

    finish js solution

--- javascript/solution.js
+++ javascript/solution.js
@@ -4,18 +4,40 @@ const assert = require("assert");
 const HOST = "localhost";
 const PORT = 7878;
 
+const STARTING_NODE = 1;
+
 const main = async () => {
-  console.log("fetching...");
-  const res = await fetch(`http://${HOST}:${PORT}/node/1`);
-  const body = await res.json();
-  assert.equal(body.score, 100);
-  return body.score;
+  let nodeIdsToGet = [STARTING_NODE];
+  let total = 0;
+
+  while (nodeIdsToGet.length > 0) {
+    console.log("executing cycle, running total:", total);
+
+    // remove from front:
+    const nextResponsePromises = nodeIdsToGet.map(async id => {
+      const res = await fetch(`http://${HOST}:${PORT}/node/${id}`);
+      return res;
+    });
+    const responses = await Promise.all(nextResponsePromises);
+    const responseBodyPromises = responses.map(async response => {
+      // TODO: filter our responses with non-200 return codes.
+      return await response.json();
+    });
+    const responseBodies = await Promise.all(responseBodyPromises);
+    nodeIdsToGet = responseBodies.reduce((acc, body) => {
+      total += body.score;
+      return acc.concat([body.left, body.right].filter(el => el !== null));
+    }, []);
+  }
+  return total;
 };
 
 console.log("starting...");
 main()
   .then(totalScore => {
     console.log("result:", totalScore);
+    // this should take 3 cycles
+    assert.equal(totalScore, 1750);
   })
   .catch(err => {
     console.log("program rejected with err:", err);
