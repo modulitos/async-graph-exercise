790130e0e16e0976758eede86ce69ee1ecdd2cde
modulitos <modulitos@users.noreply.github.com>
2020-08-17 22:33:59 -0700

    working solution, but still not ideal

--- javascript/solution.js
+++ javascript/solution.js
@@ -8,36 +8,47 @@ const STARTING_NODE = 1;
 
 const main = async () => {
   let nodeIdsToGet = [STARTING_NODE];
+  let nodeIdsRetrieved = new Set();
+  let promises = [];
   let total = 0;
 
-  while (nodeIdsToGet.length > 0) {
-    console.log("executing cycle, running total:", total);
+  while (true) {
+    if (nodeIdsToGet.length > 0) {
+      // create a new promise, add to end of promises queue
+      const numNodesToProcess = nodeIdsToGet.length;
+      nodesToProcess = nodeIdsToGet.slice(0, numNodesToProcess);
+      nodeIdsToGet = nodeIdsToGet.slice(numNodesToProcess);
 
-    const nextResponsePromises = nodeIdsToGet.map(async id => {
-      const res = await fetch(`http://${HOST}:${PORT}/node/${id}`);
-      return res;
-    });
-    const responses = await Promise.all(nextResponsePromises);
-    const responseBodyPromises = responses.map(async response => {
-      // TODO: filter our responses with non-200 return codes.
-      return await response.json();
-    });
-    const responseBodies = await Promise.all(responseBodyPromises);
-    nodeIdsToGet = responseBodies.reduce((acc, body) => {
-      total += body.reward;
-      return acc.concat([body.left, body.right].filter(el => el !== null));
-    }, []);
+      const nextPromises = nodesToProcess.map(async nextNodeId => {
+        // TODO: check if nextNodeIds is already in nodeIdsRetrieved.
+        const res = await fetch(`http://${HOST}:${PORT}/node/${nextNodeId}`);
+        const body = await res.json();
+        total += body.reward;
+        return body.children;
+      });
+      promises = promises.concat(nextPromises);
+    }
+    if (promises.length > 0) {
+      // pop off promise from front of queue, await it.
+      let nextPromise = promises.shift();
+      // TODO: we can afford to wait on this node, when there are
+      // other requests to process... Is there a way to fetch all of
+      // our promises, and process them as they return? (not wait for
+      // all of them to return)
+      let nextNodeIds = await nextPromise;
+      nextNodeIds.forEach(child => nodeIdsToGet.push(child));
+    }
+    if (promises.length === 0 && nodeIdsToGet.length == 0) {
+      return total;
+    }
   }
-  return total;
 };
 
 console.log("starting...");
 main()
   .then(total => {
     console.log("result:", total);
-    // this should take 4 cycles, each taking [1, 1, 5, 5] seconds to process
-
-    // TODO: should take less than 10 seconds to process
+    // TODO: this should take 4 cycles, each taking [1, 1, 5, 5] seconds to process
     assert.equal(total, 3850);
   })
   .catch(err => {
