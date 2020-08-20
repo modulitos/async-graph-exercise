7444dfc4f0d0af68616b9c214f70e5bbb3a33522
modulitos <modulitos@users.noreply.github.com>
2020-08-19 23:42:39 -0700

    most efficient solution created

--- javascript/solution.js
+++ javascript/solution.js
@@ -9,36 +9,34 @@ const STARTING_NODE = 1;
 const main = async () => {
   let nodeIdsToGet = [STARTING_NODE];
   let nodeIdsRetrieved = new Set();
-  let promises = [];
+  let idToPromises = {};
   let total = 0;
 
   while (true) {
     if (nodeIdsToGet.length > 0) {
-      // create a new promise, add to end of promises queue
-      const numNodesToProcess = nodeIdsToGet.length;
-      nodesToProcess = nodeIdsToGet.slice(0, numNodesToProcess);
-      nodeIdsToGet = nodeIdsToGet.slice(numNodesToProcess);
-
-      const nextPromises = nodesToProcess.map(async nextNodeId => {
-        // TODO: check if nextNodeIds is already in nodeIdsRetrieved.
-        const res = await fetch(`http://${HOST}:${PORT}/node/${nextNodeId}`);
-        const body = await res.json();
-        total += body.reward;
-        return body.children;
-      });
-      promises = promises.concat(nextPromises);
-    }
-    if (promises.length > 0) {
-      // pop off promise from front of queue, await it.
-      let nextPromise = promises.shift();
-      // TODO: we can afford to wait on this node, when there are
-      // other requests to process... Is there a way to fetch all of
-      // our promises, and process them as they return? (not wait for
-      // all of them to return)
-      let nextNodeIds = await nextPromise;
-      nextNodeIds.forEach(child => nodeIdsToGet.push(child));
-    }
-    if (promises.length === 0 && nodeIdsToGet.length == 0) {
+      const nextPromises = nodeIdsToGet
+        .map(async nodeId => {
+          // TODO: check if nextNodeIds is already in nodeIdsRetrieved.
+          const res = await fetch(`http://${HOST}:${PORT}/node/${nodeId}`);
+          const body = await res.json();
+          total += body.reward;
+          // return body.children;
+          body.children.forEach(child => nodeIdsToGet.push(child));
+          nodeIdsRetrieved.add(nodeId);
+          delete idToPromises[nodeId];
+          return nodeId;
+        })
+        .reduce((acc, promise, i) => {
+          acc[nodeIdsToGet[i]] = promise;
+          return acc;
+        }, {});
+      nodeIdsToGet = [];
+      Object.assign(idToPromises, nextPromises);
+    } else if (Object.values(idToPromises).length > 0) {
+      const nodeId = await Promise.race(Object.values(idToPromises));
+      console.log("promise.race result, nodeId:", nodeId);
+    } else {
+      await Promise.all(Object.values(idToPromises));
       return total;
     }
   }
@@ -48,7 +46,19 @@ console.log("starting...");
 main()
   .then(total => {
     console.log("result:", total);
-    // TODO: this should take 4 cycles, each taking [1, 1, 5, 5] seconds to process
+    // To process most efficiently, this will have the following requests in flight:
+
+    // | time (sec) | nodes being processed           |
+    // |------------+---------------------------------|
+    // |          1 | 1                               |
+    // |          2 | 5, 3                            |
+    // |          3 | 6, 7, 9, 10                     |
+    // |          4 | 6, 101, 102, 103, 104, 105, 106 |
+    // |          5 | 6, 101, 102, 103, 104, 105, 106 |
+    // |          6 | 6, 101, 102, 103, 104, 105, 106 |
+    // |          7 | 6, 101, 102, 103, 104, 105, 106 |
+    // |          8 | 101, 102, 103, 104, 105, 106    |
+
     assert.equal(total, 3850);
   })
   .catch(err => {
