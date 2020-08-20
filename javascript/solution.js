2bc487badc8334fc8e8e799ff85f777108f7b743
modulitos <modulitos@users.noreply.github.com>
2020-08-20 00:13:54 -0700

    updates

--- javascript/solution.js
+++ javascript/solution.js
@@ -6,32 +6,66 @@ const PORT = 7878;
 
 const STARTING_NODE = 1;
 
+// This solution leverages concurrency to calculate the solution in
+// the shortest amount of time possible. It calculates the result in 8
+// seconds, assuming no additional network latency. To process our
+// nodes most efficiently, we will have the following requests in
+// flight:
+
+// | time (sec) | nodes requests being processed  |
+// |------------+---------------------------------|
+// |          1 | 1                               |
+// |          2 | 5, 3                            |
+// |          3 | 6, 7, 9, 10                     |
+// |          4 | 6, 101, 102, 103, 104, 105, 106 |
+// |          5 | 6, 101, 102, 103, 104, 105, 106 |
+// |          6 | 6, 101, 102, 103, 104, 105, 106 |
+// |          7 | 6, 101, 102, 103, 104, 105, 106 |
+// |          8 | 101, 102, 103, 104, 105, 106    |
+
 const main = async () => {
-  let nodeIdsToGet = [STARTING_NODE];
+  // let nodeIdsToGet = [STARTING_NODE];
+  let nodeIdsToGet = new Set([STARTING_NODE]);
   let nodeIdsRetrieved = new Set();
   let idToPromises = {};
   let total = 0;
 
   while (true) {
-    if (nodeIdsToGet.length > 0) {
-      const nextPromises = nodeIdsToGet
-        .map(async nodeId => {
-          // TODO: check if nextNodeIds is already in nodeIdsRetrieved.
+    if (nodeIdsToGet.size > 0) {
+      const idToNextPromises = [...nodeIdsToGet].reduce((acc, nodeId) => {
+        // delete the item from nodeIdsToGet, as it has been converted to a promise at this point.
+        nodeIdsToGet.delete(nodeId);
+        const promise = (async nodeId => {
+          if (nodeIdsRetrieved.has(nodeId)) {
+            // Skip processing nodes that have already been fetched.
+            return null;
+          }
           const res = await fetch(`http://${HOST}:${PORT}/node/${nodeId}`);
           const body = await res.json();
+          // Increment the reward:
           total += body.reward;
-          // return body.children;
-          body.children.forEach(child => nodeIdsToGet.push(child));
+          // Add the node's children to the queue:
+          body.children.forEach(child => nodeIdsToGet.add(child));
+          // Mark the node as having already been retrieved.
           nodeIdsRetrieved.add(nodeId);
+          // Remove the fetched node from our in-flight promises:
           delete idToPromises[nodeId];
           return nodeId;
-        })
-        .reduce((acc, promise, i) => {
-          acc[nodeIdsToGet[i]] = promise;
-          return acc;
-        }, {});
-      nodeIdsToGet = [];
-      Object.assign(idToPromises, nextPromises);
+        })(nodeId);
+        acc[nodeId] = promise;
+        return acc;
+      }, {});
+
+      // All of the pending node id's have been converted to Promises, so clear the queue.
+      // nodeIdsToGet = new Set();
+      // console.log("idToNextPromises:", idToNextPromises);
+      // .reduce((acc, promise, i) => {
+      //   acc[nodeIdsToGet[i]] = promise;
+      //   return acc;
+      // }, {});
+      // nodeIdsToGet = [];
+      // Add the new promises to our mapping.
+      Object.assign(idToPromises, idToNextPromises);
     } else if (Object.values(idToPromises).length > 0) {
       const nodeId = await Promise.race(Object.values(idToPromises));
       console.log("promise.race result, nodeId:", nodeId);
@@ -46,19 +80,6 @@ console.log("starting...");
 main()
   .then(total => {
     console.log("result:", total);
-    // To process most efficiently, this will have the following requests in flight:
-
-    // | time (sec) | nodes being processed           |
-    // |------------+---------------------------------|
-    // |          1 | 1                               |
-    // |          2 | 5, 3                            |
-    // |          3 | 6, 7, 9, 10                     |
-    // |          4 | 6, 101, 102, 103, 104, 105, 106 |
-    // |          5 | 6, 101, 102, 103, 104, 105, 106 |
-    // |          6 | 6, 101, 102, 103, 104, 105, 106 |
-    // |          7 | 6, 101, 102, 103, 104, 105, 106 |
-    // |          8 | 101, 102, 103, 104, 105, 106    |
-
     assert.equal(total, 3850);
   })
   .catch(err => {
